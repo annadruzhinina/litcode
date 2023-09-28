ea6c9cc35954072514d515041d7a5490ffba8245
anna <anna@rcbd.org>
2023-09-27 22:37:41 -0400

    Complete task

--- ConvertSortedArraytoBinary.js
+++ ConvertSortedArraytoBinary.js
@@ -1,10 +1,16 @@
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
-
-function sortedArrayToBST(nums) {
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
   if (!nums.length) return null;
 
   let midIdx = Math.floor(nums.length / 2);
@@ -14,6 +20,6 @@ function sortedArrayToBST(nums) {
   root.right = sortedArrayToBST(nums.slice(midIdx + 1));
 
   return root;
-}
+};
 
 console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
